2b930d4d9425cb1d98fd4d75b2ae5ad763dccdd4
rafael Gutierrez Gaspar <rafarobertgu@gmail.com>
2021-02-17 13:42:38 -0400

    creacion de middleware y frontend

--- middleware/build/scripts/sequelize-cli-mac.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# para deshacer una migracion con sequelize
-# sequelize db:migrate:undo --name 007-create-co-users.js
-# para migrar una table con sequelize
-# sequelize db:migrate --from 007-create-co-users.js --to 007-create-co-users.js
--- middleware/build/scripts/sequelize-cli-ubuntu.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#npx sequelize-cli db:migrate:undo:all
-#npx sequelize-cli db:migrate
-
-# para deshacer una migracion con sequelize
-#npx sequelize-cli db:migrate:undo --name 007-create-co-users.js
-
-# para migrar una table con sequelize
-# sequelize db:migrate --from 007-create-co-users.js --to 007-create-co-users.js
