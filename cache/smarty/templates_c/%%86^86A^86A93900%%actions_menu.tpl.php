10bd61c3552462ee7af84308878c6ebe8d0e1532
rafael Gutierrez Gaspar <rafarobertgu@gmail.com>
2021-02-21 10:04:42 -0400

    arreglo sistema pilatsrl, cache ignorado

---
+++ cache/smarty/templates_c/%%86^86A^86A93900%%actions_menu.tpl.php
@@ -0,0 +1,22 @@
+<?php /* Smarty version 2.6.31, created on 2021-02-21 14:57:41
+         compiled from themes/sp_theme/include/MySugar/tpls/actions_menu.tpl */ ?>
+<ul class="dropdown-menu tab-actions">
+    <?php if (! $this->_tpl_vars['lock_homepage']): ?>
+        <li>
+            <a class="addDashlets"  data-toggle="modal" data-target=".modal-add-dashlet"><?php echo $this->_tpl_vars['lblAddDashlets']; ?>
+</a>
+        </li>
+        <li>
+            <a class="addDashboard"  data-toggle="modal" data-target=".modal-add-dashboard">
+                <span><?php echo $this->_tpl_vars['lblAddTab']; ?>
+</span>
+            </a>
+        </li>
+        <li>
+            <a class="addDashboard"  data-toggle="modal" data-target=".modal-edit-dashboard">
+                <span><?php echo $this->_tpl_vars['app']['LBL_EDIT_TAB']; ?>
+</span>
+            </a>
+        </li>
+    <?php endif; ?>
+</ul>
