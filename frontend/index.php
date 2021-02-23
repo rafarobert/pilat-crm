269e10463ceb4ce57ab47aba321308b4dcc16dd5
rafael Gutierrez Gaspar <rafarobertgu@gmail.com>
2021-02-23 13:05:43 -0400

    arreglo en modulo suite crm constructor

--- frontend/index.php
+++ frontend/index.php
@@ -19,7 +19,7 @@ $cookie_user_loggedin = "userLogguedIn";
 $cookie_user_loggedin_value = isset($_SESSION['authenticated_user_id']) ? $_SESSION['authenticated_user_id'] : '';
 setcookie($cookie_user_loggedin, $cookie_user_loggedin_value, time() + (86400 * 30), "/"); // 86400 = 1 day
 
-//if ($_REQUEST['module'] == 'Opportunities' || $_REQUEST['module'] == 'Leads' || $_REQUEST['module'] == 'Accounts' || $_REQUEST['module'] == 'Contacts') {
+if ($_REQUEST['module'] != 'ModuleBuilder') {
   echo '<script type="text/javascript" src="frontend/index.js"></script>';
-//}
+}
 
