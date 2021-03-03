a091573f9935b29e5bba1403cb7a2c26609b869c
rafael Gutierrez Gaspar <rafarobertgu@gmail.com>
2021-03-03 09:34:46 -0400

    nombre duplicados

--- middleware/api/src/crm/lead/lead.service.js
+++ middleware/api/src/crm/lead/lead.service.js
@@ -104,7 +104,7 @@ class LeadService {
 				if (newLead) {
 					let alreadyLeads;
 
-					alreadyLeads = await models.sequelize.leads.findAll({where:{[Op.or]:[{phone_mobile: newLead.phone_mobile+''},{first_name: newLead.first_name+''},{last_name: newLead.last_name+''}]}});
+					alreadyLeads = await models.sequelize.leads.findAll({where:{[Op.and]:[{phone_mobile: newLead.phone_mobile+''},{first_name: newLead.first_name+''},{last_name: newLead.last_name+''}]}});
 
 					if (alreadyLeads && alreadyLeads.length) {
 						objLead = alreadyLeads;
