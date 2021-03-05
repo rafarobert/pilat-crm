91dcfad9a3d518441bb622673a07ee2ed7f8e3d8
rafael Gutierrez Gaspar <rafarobertgu@gmail.com>
2021-03-05 07:44:52 -0400

    codigo extra middleware

--- crm-pilat/middleware/public/js/es/shared/common.js
+++
@@ -1,3 +0,0 @@
-function formatDate(value){
-	return value ? Ext.Date.dateFormat(value, 'yy/m/d H:i:s') : Ext.Date.dateFormat(new Date(), 'yy/m/d H:i:s');
-}
--- crm-pilat/middleware/public/pilatsrl/extjs/app/controller/Main.js
+++
@@ -1,3 +0,0 @@
-Ext.define('es.controller.Main', {
-    extend: 'Ext.app.Controller'
-});
